2f613e832c2cc0de6804842e4fc75ad254122229
Gad Josue Alva Castromonte <josuealva920@gmail.com>
2024-10-12 19:52:46 -0500

    impl. logout async front

--- js/principal.js
+++ js/principal.js
@@ -1,13 +1,24 @@
 let msgSuccess;
 
 window.addEventListener('load', function(){
-    //referenciar elemntos de la pagina
+    // Referenciar elementos de la página
     msgSuccess = document.getElementById('msgSuccess');
 
-    //recuperar nombre del usuario
-    const result = JSON.parse(this.localStorage.getItem('result'));
-    console.log('Resultado del servidor : ', result);
-    mostrarAlerta(result.nombreUsuario);
+    // Recuperar nombre del usuario
+    const result = JSON.parse(localStorage.getItem('result'));
+    if (result) {
+        console.log('Resultado del servidor : ', result);
+        mostrarAlerta(result.nombreUsuario);
+
+        const logoutLink = document.getElementById('logout_link');
+        logoutLink.addEventListener('click', function(){
+            alert('Saliendo de la aplicación');
+            logout(result.nombreUsuario);
+        });
+    } else {
+        console.error('Error: No se pudo recuperar el resultado del localStorage');
+        mostrarAlerta('Error: No se pudo recuperar el resultado del localStorage');
+    }
 });
 
 function mostrarAlerta(mensaje){
@@ -18,4 +29,32 @@ function mostrarAlerta(mensaje){
 function ocultarAlerta(){
     msgSuccess.innerHTML = '';
     msgSuccess.style.display = 'none';
+}
+
+async function logout(nombreUsuario){
+    const url = 'http://localhost:8082/login/logout-async';
+    const data = {
+        nombreUsuario: nombreUsuario
+    };
+    console.log('Data a enviar: ', data);
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        console.log('Respuesta del servidor: ', result);
+        if (result.codigo == '00') {
+            window.location.replace('index.html');
+        } else {
+            mostrarAlerta('Error: Ocurrió un problema en el logout');
+        }
+    } catch (error) {
+        console.error('Error: Ocurrió un problema no identificado', error);
+        mostrarAlerta('Error: Ocurrió un problema no identificado');
+    }
 }
